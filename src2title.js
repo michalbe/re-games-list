73a003418924169f7ff1e52f05cdf60b96cbbfdb
Michał Budzyński <michal@virtualdesign.pl>
2014-12-06 01:01:00 +0100

    :mute: replacer implemented

--- src2title.js
+++ src2title.js
@@ -1,6 +1,16 @@
 'use strict';
 //pics/ps1/residentevil_eu.jpg
+
+var changes = {
+  'residentevil': 'Resident Evil ',
+  '.jpg': ''
+}
 module.exports = function(src) {
-  src = src.split('/').pop().replace('.jpg', '');
+  src = src.split('/').pop();
+
+  for (var i in changes) {
+    src = src.replace(i, changes[i]);
+  }
+
   return src;
 };
