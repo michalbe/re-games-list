e51525e597ba7a2b4fbd29db4e28f30d74a23d41
Michał Budzyński <michal@virtualdesign.pl>
2014-12-06 01:01:00 +0100

    :mahjong: renamed src2title to data-form-src

---
+++ data-from-src.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var regions = {
+  'ger': 4,
+  'eu': 1,
+  'usa': 2,
+  'us': 2,
+  'jap': 3
+};
+
+var changes = {
+  '.jpg': '',
+  'directorscut': 'Director\'s Cut',
+  'residentevil': 'Resident Evil',
+  'gunsurv': 'Gun Survivor',
+  'wl': 'White Label',
+  'whitelabel': 'White Label',
+  'biohazard': 'Biohazard',
+  'dc': '+ Dino Crisis Demo',
+  'plat': 'Platinum',
+  'jewelc': 'Jewel Case',
+  ' ase': '',
+  'longbox': 'Long Box',
+  'valueseries': 'Value Series',
+  'dsv': 'Dualshock',
+  'ds': 'Dualshock',
+  'surv': 'Survivor',
+  'capk': 'CapKore',
+  ' inum': ''
+};
+
+var getRegion = function(title) {
+  for (var i in regions) {
+    if (title.indexOf(i) > -1) {
+      return regions[i];
+    }
+  }
+};
+
+module.exports = function(src) {
+  src = src.split('/').pop();
+  var region = getRegion(src);
+
+  for (var i in regions) {
+    src = src.replace('_' + i, '');
+  }
+
+  for (i in changes) {
+    src = src.replace(i, ' ' + changes[i] + ' ');
+  }
+
+
+  src = src.replace('_', ' ');
+  src = src.replace(/[ ]{2,}/g, ' ');
+
+  return {
+    region: region,
+    title: src.trim()
+  };
+};
