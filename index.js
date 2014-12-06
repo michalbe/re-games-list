e04ac43a87249813fa2212c552fe0e4c96e67b24
Michał Budzyński <michal@virtualdesign.pl>
2014-12-06 01:01:00 +0100

    :sunflower: get site content

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var request = require('request');
+var url = 'http://www.michaelchandler.residentevilcenter.net';
+
+var platforms = [];
+
+request(url, function(err, req, body) {
+  console.log(body);
+});
