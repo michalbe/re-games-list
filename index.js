9b0b50946d0dcb9d3e83752163a63c6eed5db33a
Michał Budzyński <michal@virtualdesign.pl>
2014-12-06 01:01:00 +0100

    :house: cheerio added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var request = require('request');
 var url = 'http://www.michaelchandler.residentevilcenter.net';
 
-var platforms = [];
+//var platforms = [];
 
 request(url, function(err, req, body) {
   console.log(body);
