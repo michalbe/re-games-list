98158a8518e8df2d2229c0f1452955bea408c400
Michał Budzyński <michal@virtualdesign.pl>
2014-12-06 01:01:00 +0100

    :evergreen_tree: filter proper platform links

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 'use strict';
 
 var request = require('request');
+var cheerio = require('cheerio');
 var url = 'http://www.michaelchandler.residentevilcenter.net';
 
+var $;
 //var platforms = [];
 
 request(url, function(err, req, body) {
-  console.log(body);
+  var title;
+  $ = cheerio.load(body, { normalizeWhitespace: true });
+
+  var platformLinks = $('a.sec').filter(function(e) {
+    title =  $(this).text();
+    return title.indexOf('RE/BH') > -1 &&
+        title.indexOf('Comics') === -1 &&
+        title.indexOf('Guides') === -1;
+  });
+
+  console.log(platformLinks.text());
 });
