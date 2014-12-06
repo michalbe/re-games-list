808f120d2f921f9a4023a05fb7e34ae43bb773df
Michał Budzyński <michal@virtualdesign.pl>
2014-12-06 01:01:00 +0100

    :articulated_lorry: get all the supported platforms

--- index.js
+++ index.js
@@ -2,21 +2,46 @@
 
 var request = require('request');
 var cheerio = require('cheerio');
-var url = 'http://www.michaelchandler.residentevilcenter.net';
+var url = 'http://www.michaelchandler.residentevilcenter.net/';
 
 var $;
-//var platforms = [];
+var platforms = [];
 
-request(url, function(err, req, body) {
-  var title;
-  $ = cheerio.load(body, { normalizeWhitespace: true });
+var getPlatforms = function(cb) {
+  request(url, function(err, req, body) {
 
-  var platformLinks = $('a.sec').filter(function(e) {
-    title =  $(this).text();
-    return title.indexOf('RE/BH') > -1 &&
-        title.indexOf('Comics') === -1 &&
-        title.indexOf('Guides') === -1;
+    if (err) {
+      cb(err);
+      return;
+    }
+
+    var element;
+    $ = cheerio.load(body, { normalizeWhitespace: true });
+
+    var platformLinks = $('a.sec').filter(function(e) {
+      element =  $(this).text();
+      return element.indexOf('RE/BH') > -1 &&
+      element.indexOf('Comics') === -1 &&
+      element.indexOf('Guides') === -1;
+    });
+
+    platformLinks.each(function() {
+      element = $(this);
+      platforms.push({
+        name: element.text().replace('RE/BH', '').trim(),
+        url: element.attr('href')
+      });
+    });
+
+    cb(null, platforms);
   });
+}
 
-  console.log(platformLinks.text());
+getPlatforms(function(err, platforms) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(platforms);
 });
